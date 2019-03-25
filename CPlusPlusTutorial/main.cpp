967c8febc7e7a0c17d32903fd3cbf351072cf040
Quoc Nguyen <quocnb.vnu@gmail.com>
2019-03-25 09:02:54 +0900

    Initial Commit

---
+++ CPlusPlusTutorial/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CPlusPlusTutorial
+//
+//  Created by Quoc Nguyen on 2019/03/25.
+//  Copyright © 2019 Quoc Nguyen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
